972ef8ea9532dc2d333cc5a3559351df2c9494af
danielmoi <dmoixy@gmail.com>
2019-05-28 10:33:18 +1000

    recursively write to new directory

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,60 @@
 extern crate soup;
 extern crate pulldown_cmark;
+extern crate walkdir;
 
 use std::fs;
 use std::error::Error;
 use pulldown_cmark::{Parser, Options, html};
+use walkdir::WalkDir;
+use std::path::Path;
 
 fn main() {
     println!("Hello there");
 
-    let contents = fs::read_to_string("./basic.md")
-      .expect("Something went wrong reading the file");
-    // println!("Contents: {}", contents);
+    let file = "./basic.md";
+    let out = Path::new("./out");
 
-    let mut options = Options::empty();
-    options.insert(Options::all());
-    let parser = Parser::new_ext(&contents, options);
+    for entry in WalkDir::new("documents") {
+      println!("--------------");
+      let entry = entry.unwrap();
+      // println!("entry: {}", entry.metadata());
+      println!("entry: {}", entry.path().display());
 
-    let mut html_buf = String::new();
-    html::push_html(&mut html_buf, parser);
+      let entry_path = Path::new(entry.path());
+      println!("entry_path: {}", entry_path.display());
+      let parent = entry_path.parent().unwrap();
+      println!("parent: {}", parent.display());
 
-    println!("HTML: {}", html_buf);
+      let new_parent_path = out.join(parent);
+      println!("new_parent_path: {}", &new_parent_path.display());
+      fs::create_dir_all(&new_parent_path);
 
-    fs::create_dir_all("./build");
-    fs::write("./build/basic.html", html_buf).expect("Unable to write file");
+      let is_file = entry_path.is_file();
+      println!("is_file: {}", is_file);
+
+      if (entry_path.is_file()) {
+        let new_entry_path = out.join(entry_path);
+        println!("new_entry_path: {}", &new_entry_path.display());
+        fs::write(new_entry_path, "HELLO").expect("Fail.......");
+      }
+    }
+
+    // let contents = fs::read_to_string(path)
+    //   .expect("Something went wrong reading the file");
+    // // println!("Contents: {}", contents);
+
+    // let mut options = Options::empty();
+    // options.insert(Options::all());
+    // let parser = Parser::new_ext(&contents, options);
+
+    // let mut html_buf = String::new();
+    // html::push_html(&mut html_buf, parser);
+
+    // println!("HTML: {}", html_buf);
+
+    // let parent_folder = file.parent();
+    // println!("parent_folder: {}", parent_folder);
+    // fs::create_dir_all("./build");
+    // fs::write("./build/basic.html", html_buf).expect("Unable to write file");
 
 }
