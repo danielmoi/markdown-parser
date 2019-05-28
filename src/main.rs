c4681043007cf9650101c64ef28fe8ba7ab1b00a
danielmoi <dmoixy@gmail.com>
2019-05-28 13:42:01 +1000

    Add footer and tidy main

--- src/main.rs
+++ src/main.rs
@@ -8,37 +8,30 @@ use walkdir::WalkDir;
 use std::path::Path;
 
 fn main() {
-    println!("Hello there");
+    println!("Starting parser");
 
     let out = Path::new("./out");
 
-    let mut header_file = fs::File::open("header.html");
-    // let mut header_contents = String::new();
     let header_contents = fs::read_to_string("header.html").unwrap();
 
+    let footer_contents = fs::read_to_string("footer.html").unwrap();
+
+
+
     for entry in WalkDir::new("documents") {
-      println!("--------------");
       let entry = entry.unwrap();
-      // println!("entry: {}", entry.metadata());
-      println!("entry: {}", entry.path().display());
-
       let entry_path = Path::new(entry.path());
-      println!("entry_path: {}", entry_path.display());
+
       let parent = entry_path.parent().unwrap();
-      println!("parent: {}", parent.display());
-
       let new_parent_path = out.join(parent);
-      println!("new_parent_path: {}", &new_parent_path.display());
-      fs::create_dir_all(&new_parent_path);
 
-      let is_file = entry_path.is_file();
-      println!("is_file: {}", is_file);
+      fs::create_dir_all(&new_parent_path)
+        .expect("Error creating directory");
 
       if entry_path.is_file() {
 
         let contents = fs::read_to_string(entry_path)
-          .expect("Something went wrong reading the file");
-        // println!("Contents: {}", contents);
+          .expect("Error reading entry file");
 
         let mut options = Options::empty();
         options.insert(Options::all());
@@ -47,22 +40,13 @@ fn main() {
         let mut html_buf = String::new();
         html_buf.push_str(&header_contents);
         html::push_html(&mut html_buf, parser);
-
+        html_buf.push_str(&footer_contents);
 
         let new_entry_path = out.join(&entry_path).with_extension("html");
-        println!("new_entry_path: {}", &new_entry_path.display());
 
-        fs::write(new_entry_path, html_buf).expect("Fail.......");
+        fs::write(new_entry_path, html_buf).expect("Error writing to html");
       }
     }
 
-
-
-    // println!("HTML: {}", html_buf);
-
-    // let parent_folder = file.parent();
-    // println!("parent_folder: {}", parent_folder);
-    // fs::create_dir_all("./build");
-    // fs::write("./build/basic.html", html_buf).expect("Unable to write file");
-
+    println!("Finished parsing");
 }
